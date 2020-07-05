1ebf8998a02afff470ae881c71e6cc5de5076910
Shashi Shekhar <Shekhars094@gmail.com>
2020-07-05 12:03:55 +0530

    pushing keypad

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -3,36 +3,42 @@ import React from "react";
 const Keypad = () => {
 	return (
 		<div>
-			<div className="container" style={{width: 50%}}>
+			<div
+				className="container"
+				style={{ width: "35%", marginTop: "30vh" }}>
 				<div className="row">
-					<div className="col-3">1</div>
-					<div className="col-3">2</div>
-					<div className="col-3">3</div>
-					<div className="col-3">4</div>
+					<div className="col-3 border text-center bg-primary">1</div>
+					<div className="col-3 border text-center bg-primary">2</div>
+					<div className="col-3 border text-center bg-primary">3</div>
+					<div className="col-3 border text-center bg-primary">4</div>
 				</div>
 				<div className="row">
-					<div className="col-3"></div>
-					<div className="col-3"></div>
-					<div className="col-3"></div>
-					<div className="col-3"></div>
+					<div className="col-3 border text-center bg-primary">5</div>
+					<div className="col-3 border text-center bg-primary">6</div>
+					<div className="col-3 border text-center bg-primary">7</div>
+					<div className="col-3 border text-center bg-primary">8</div>
 				</div>
 				<div className="row">
-					<div className="col-3"></div>
-					<div className="col-3"></div>
-					<div className="col-3"></div>
-					<div className="col-3"></div>
+					<div className="col-3 border text-center bg-primary">9</div>
+					<div className="col-3 border text-center bg-primary">0</div>
+					<div className="col-3 border text-center bg-primary">
+						CE
+					</div>
+					<div className="col-3 border text-center bg-primary">C</div>
 				</div>
 				<div className="row">
-					<div className="col-3"></div>
-					<div className="col-3"></div>
-					<div className="col-3"></div>
-					<div className="col-3"></div>
+					<div className="col-3 border text-center bg-primary">+</div>
+					<div className="col-3 border text-center bg-primary">-</div>
+					<div className="col-3 border text-center bg-primary">*</div>
+					<div className="col-3 border text-center bg-primary">
+						&#247;
+					</div>
 				</div>
 				<div className="row">
-					<div className="col-3"></div>
-					<div className="col-3"></div>
-					<div className="col-3"></div>
-					<div className="col-3"></div>
+					<div className="col-3 border text-center bg-primary">=</div>
+					<div className="col-3 border text-center bg-primary">(</div>
+					<div className="col-3 border text-center bg-primary">)</div>
+					<div className="col-3 border text-center bg-primary">.</div>
 				</div>
 			</div>
 		</div>
